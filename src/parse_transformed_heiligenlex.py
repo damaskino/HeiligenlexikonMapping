0d9051ac7a866b99f219f1f731ac11878d64c464
chen <chen.li@posteo.net>
2023-06-25 20:30:18 +0200

    Add exploratory notebook, replace pickle with joblib

--- src/parse_transformed_heiligenlex.py
+++ src/parse_transformed_heiligenlex.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import time
-import pickle
+import joblib
 import os
 import sys
 
@@ -9,13 +9,13 @@ HLEX_SOUP_PICKLE = 'hlex_soup.pickle'
 def load_transformed_hlex_to_soup():
     hlex_xml_path = '../data/Heiligenlex-1858.xml'
     with open(hlex_xml_path, 'r') as hlex:
-        soup = BeautifulSoup(hlex, 'lxml')
+        soup = BeautifulSoup(hlex, features="xml")
         return soup
 
 def pickle_it(object_to_pickle, path: str):
     print("Attempting to pickle...")
     with open(path, 'wb') as target_file:
-        pickle.dump(obj=object_to_pickle, file=target_file, protocol=pickle.HIGHEST_PROTOCOL)
+        joblib.dump(value=object_to_pickle, filename=target_file)
 
 def timing_wrapper(func, param):
     start = time.time()
@@ -30,14 +30,13 @@ def timing_wrapper(func, param):
 
 if __name__ == '__main__':
     hlex_soup = None
-    sys.setrecursionlimit(sys.getrecursionlimit()*50)
-    print("Attempting with recursionlimit:", sys.getrecursionlimit())
-    #TODO: Add a check to see if pickle is corrupt
+    #sys.setrecursionlimit(sys.getrecursionlimit()*50)
+    #print("Attempting with recursionlimit:", sys.getrecursionlimit())
+    #TODO: Add a check to see if pickle file is corrupt
     if os.path.isfile('tmp/'+HLEX_SOUP_PICKLE):
         print("Pickle found, loading...")
         with open('tmp/' + HLEX_SOUP_PICKLE, 'rb') as pickle_file:
-            pickle.load(pickle_file)
-            hlex_soup = timing_wrapper(pickle.load, pickle_file)
+            hlex_soup = timing_wrapper(joblib.load, pickle_file)
     else:
         print("No pickle found, loading from XML...")
         hlex_soup = timing_wrapper(load_transformed_hlex_to_soup, None)
