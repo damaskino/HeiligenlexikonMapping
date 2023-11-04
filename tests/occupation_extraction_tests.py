85e89e041420c730f5ec0d8fbced292455c2a880
chen <chenzhongchen@gmail.com>
2023-11-04 17:04:34 +0100

    add script for extracting entries only, wip test stub for occupation extraction

---
+++ tests/occupation_extraction_tests.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class OccupationTestCase(unittest.TestCase):
+
+    def setUpClass(cls) -> None:
+
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
