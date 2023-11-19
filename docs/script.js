03d4e532e3e6fc5893b285345dbd327224e1c816
chen <chenzhongchen@gmail.com>
2023-11-19 23:16:11 +0100

    attempt to use datatables for nicer table representation

--- docs/script.js
+++ docs/script.js
@@ -1,26 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-  fetch('test.json') // Replace 'data.json' with your JSON file path
-    .then(response => response.json())
-    .then(data => {
-      const table = document.getElementById('dataTable');
-
-      // Create table headers from the keys in the first object
-      const headers = Object.keys(data[0]);
-      const headerRow = table.insertRow();
-      headers.forEach(header => {
-        const th = document.createElement('th');
-        th.textContent = header;
-        headerRow.appendChild(th);
-      });
-
-      // Populate table with data
-      data.forEach(obj => {
-        const row = table.insertRow();
-        headers.forEach(header => {
-          const cell = row.insertCell();
-          cell.textContent = obj[header];
-        });
-      });
-    })
-    .catch(error => console.error('Error fetching the data:', error));
+$(document).ready(function() {
+  $('#dataTable').DataTable({
+    ajax: {
+      url: 'test.json', // Replace 'data.json' with your JSON file path
+      dataSrc: ''
+    },
+    columns: [
+      { data: 'SaintName' },
+      { data: 'CanonizationStatus' }
+      { data: 'EntryLength' }
+      // Define more columns as needed
+    ]
+  });
 });
