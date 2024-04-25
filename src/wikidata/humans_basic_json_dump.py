be06e19bdd1910a977af6b298c1caf7724fce949
damaskino <chenzhongchen@googlemail.com>
2024-04-25 12:30:07 +0900

    update requirements.txt, filter wikidata json dump for humans

--- src/wikidata/humans_basic_json_dump.py
+++ src/wikidata/humans_basic_json_dump.py
@@ -1,3 +1,4 @@
+import platform
 import time
 
 from qwikidata.entity import WikidataItem
@@ -8,7 +9,8 @@ from qwikidata.utils import dump_entities_to_json
 
 P_CANONIZATION = "P411"
 Q_SAINT = "Q43115"
-
+P_INSTANCE_OF = "P31"
+Q_HUMAN = "Q5"
 
 def has_canonization_status(item: WikidataItem, truthy: bool = True) -> bool:
     """Return True if the Wikidata Item has a canonization status."""
@@ -17,6 +19,8 @@ def has_canonization_status(item: WikidataItem, truthy: bool = True) -> bool:
     else:
         claim_group = item.get_claim_group(P_CANONIZATION)
 
+    item.get
+
     occupation_qids = [
         claim.mainsnak.datavalue.value["id"]
         for claim in claim_group
@@ -24,33 +28,55 @@ def has_canonization_status(item: WikidataItem, truthy: bool = True) -> bool:
     ]
     return Q_SAINT in occupation_qids
 
+def is_human(item: WikidataItem, truthy: bool = True) -> bool:
+
+
+    if truthy:
+        claim_group = item.get_truthy_claim_group(P_INSTANCE_OF)
+    else:
+        claim_group = item.get_claim_group(P_INSTANCE_OF)
+
+    occupation_qids = [
+        claim.mainsnak.datavalue.value["id"]
+        for claim in claim_group
+        if claim.mainsnak.snaktype == "value"
+    ]
+
+    return Q_HUMAN in occupation_qids
 
 # create an instance of WikidataJsonDump
-wjd_dump_path = "D:\\0-MasterData\\latest-all.json.gz"
+
+#Workaround to deal with different dev setups
+wjd_dump_path = ""
+if platform.system()=="Windows":
+    wjd_dump_path = "D:\\0-MasterData\\latest-all.json.gz"
+else:
+    wjd_dump_path = "/Users/chen/0-MasterData/latest-all.json.bz2"
 wjd = WikidataJsonDump(wjd_dump_path)
 
-# create an iterable of WikidataItem representing saints
-saints = []
+# create an iterable of WikidataItem representing humans
+humans = []
 t1 = time.time()
 for ii, entity_dict in enumerate(wjd):
     entity = entity_dict
     try:
         if entity_dict["type"] == "item":
             entity = WikidataItem(entity_dict)
-            if has_canonization_status(entity):
-                saints.append(entity)
-                print(entity)
+            #if has_canonization_status(entity):
+            if is_human(entity):
+                humans.append(entity)
+                #print(entity)
 
         if ii % 1000 == 0:
             t2 = time.time()
             dt = t2 - t1
             print(
-                "found {} saints among {} entities [entities/s: {:.2f}]".format(
-                    len(saints), ii, ii / dt
+                "found {} humans among {} entities [entities/s: {:.2f}]".format(
+                    len(humans), ii, ii / dt
                 )
             )
 
-        if len(saints) > 10000:
+        if len(humans) > 10000:
             break
     except Exception:
         print("Exception at entity:", entity_dict)
@@ -58,6 +84,6 @@ for ii, entity_dict in enumerate(wjd):
 # write the iterable of WikidataItem to disk as JSON
 out_fname = "filtered_entities.json"
 print("Dumping to Json...")
-dump_entities_to_json(saints, out_fname)
+dump_entities_to_json(humans, out_fname)
 
 
