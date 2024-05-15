09b69e40cee9df44c763f7812f28187005e32ba5
damaskino <chenzhongchen@gmail.com>
2024-05-15 14:24:47 +0900

    get relevant fields from json, add stub for heiligenlexikon comparison

--- src/wikidata/process_json_from_db.py
+++ src/wikidata/process_json_from_db.py
@@ -14,14 +14,32 @@ saints = cursor.execute('SELECT * from hundred_saints')
 #labels: labels, i.e. the name of the entry in different languages
 #descriptions: descriptions for the different languages
 #aliases: different names for different languages, multiples for each language possible
-#claims: contains different statements about the object, most likely properties(?), i.e. "is saint", "is human"
+#claims: this is where the "meat" is
+# contains different statements about the object could be synonmous with claims it seems, most likely properties(?), i.e. "is saint", "is human"
+# each statement consists of a mainsnak (who invents these words??) which is the value of the property it's associated with
+# (think: Property "is a" has the value/snak "elefant")s and two optional lists of qualifier snaks and references
+# a statement is always associated with a property (P<number of property>) and there can be multiple statements about the same property in an item
+# e.g. the
 #sitelinks: links to other sites describing the objects, usually wikipedia-sites only it seems
 
 
 def parse_json_content(json_saint: dict):
     print(json_saint)
+    wikidata_id = json_saint['id']
+    labels = json_saint['labels']
+    descriptions = json_saint['descriptions']
+    aliases = json_saint['aliases']
+    claims = json_saint['claims']
+    sitelinks = json_saint['sitelinks']
 
     json_saint.keys()
+
+#TODO: check how many have a birthdate available, and sort accordingly, could help in processing time
+
+#Loads the entries from the heiligenlexikon for comparison with the wikidata entries from the database
+def load_hlex_entries:
+    pass
+
 
 for id,content in saints:
     print(id)
