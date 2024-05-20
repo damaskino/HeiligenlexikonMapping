3ee9c881d976fc7f514827855447a40ccccb49b5
chen <chenzhongchen@gmail.com>
2024-05-20 09:40:48 +0900

    add wikidata id -> date mapping

--- src/wikidata/build_wikidata_date_dict.py
+++ src/wikidata/build_wikidata_date_dict.py
@@ -6,11 +6,23 @@ from qwikidata.linked_data_interface import get_entity_dict_from_api
 
 # P31 instance of, point in time with respect to current time frame, P156 followed by, i.e. "the day after that is..."
 
-current_date_id = "Q2150"
+january1st_id = "Q2150"
+current_date_id = january1st_id
 
+wiki_id_date_dict = {}
 while True:
     # update date id
     date_dict = get_entity_dict_from_api(current_date_id)
     current_date_id = date_dict['claims']['P31'][0]['qualifiers']['P156'][0]['datavalue']['value']['id']
-    print(date_dict['labels']['en']['value'])
+    string_representation = date_dict['labels']['en']['value']
+
+    wiki_id_date_dict[current_date_id] = string_representation
     print(current_date_id)
+    if current_date_id == january1st_id:
+        break
+
+with open('date_wikidata_id_mapping.txt', 'w') as f:
+    string_to_write = ""
+    for entry in wiki_id_date_dict:
+        string_to_write += entry + ";" + wiki_id_date_dict[entry] + "\n"
+    f.write(string_to_write)
