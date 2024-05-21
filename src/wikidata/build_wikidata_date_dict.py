bbb3a25c2c2cc510841ae94fa103342f0c495c69
chen <chenzhongchen@gmail.com>
2024-05-21 13:21:55 +0900

    refactor wikidata/date mapping to account for eastern orthodox liturgic calendar, add orthodox calendar mapping

--- src/wikidata/build_wikidata_date_dict.py
+++ src/wikidata/build_wikidata_date_dict.py
@@ -1,28 +1,49 @@
 # Since every day of the year in Wikidata has it's own entry/id, we will try to build a dictionary of those days first
 
 # Starting from January 1 / Q2150
-from qwikidata.entity import WikidataItem, WikidataLexeme, WikidataProperty
 from qwikidata.linked_data_interface import get_entity_dict_from_api
 
-# P31 instance of, point in time with respect to current time frame, P156 followed by, i.e. "the day after that is..."
+P156
+followed
+by, i.e.
+"the day after that is..."
 
-january1st_id = "Q2150"
-current_date_id = january1st_id
 
-wiki_id_date_dict = {}
-while True:
-    # update date id
-    date_dict = get_entity_dict_from_api(current_date_id)
-    current_date_id = date_dict['claims']['P31'][0]['qualifiers']['P156'][0]['datavalue']['value']['id']
-    string_representation = date_dict['labels']['en']['value']
+def map_wikidata_calender(first_day_of_year_id: str) -> dict[str:str]:
+    current_date_id = first_day_of_year_id
 
-    wiki_id_date_dict[current_date_id] = string_representation
-    print(current_date_id)
-    if current_date_id == january1st_id:
-        break
+    wiki_id_date_dict = {}
+    # Going through the gregorian calender year
+    while True:
+        # update date id
+        date_dict = get_entity_dict_from_api(current_date_id)
+        string_representation = date_dict['labels']['en']['value']
+        print(current_date_id, ' : ', string_representation)
 
-with open('date_wikidata_id_mapping.txt', 'w') as f:
-    string_to_write = ""
-    for entry in wiki_id_date_dict:
-        string_to_write += entry + ";" + wiki_id_date_dict[entry] + "\n"
-    f.write(string_to_write)
+        # P31 is "instance of" property, first element of that is assumed to be "point in time with respect to recurrent timeframe"
+        # P156 is "followed by" property, which we use to get to the next date
+        current_date_id = date_dict['claims']['P31'][0]['qualifiers']['P156'][0]['datavalue']['value']['id']
+
+        wiki_id_date_dict[current_date_id] = string_representation
+
+        if current_date_id == first_day_of_year_id:
+            break
+
+    return wiki_id_date_dict
+
+
+def write_date_dict_to_file(wiki_id_date_dict: dict, file_name: str):
+    with open(file_name, 'w') as f:
+        string_to_write = ""
+        for entry in wiki_id_date_dict:
+            string_to_write += entry + ";" + wiki_id_date_dict[entry] + "\n"
+        f.write(string_to_write)
+
+
+# gregorian calendar starts with Q2150
+gregorian_wiki_id_date_dict = map_wikidata_calender("Q2150")
+write_date_dict_to_file(gregorian_wiki_id_date_dict, file_name="gregorian_wiki_date_mapping.txt")
+
+# orthodox eastern liturgical calendar starts with Q13376314
+orthodox_wiki_id_date_dict = map_wikidata_calender("Q13376314")
+write_date_dict_to_file(orthodox_wiki_id_date_dict, file_name="orthodox_wiki_date_mapping.txt")
