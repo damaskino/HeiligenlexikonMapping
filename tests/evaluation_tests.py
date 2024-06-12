50672185cc61b84a64f56806be9f82c3ebbbae5b
chen <chenzhongchen@gmail.com>
2024-06-12 13:45:33 +0900

    Adds WIP tests for metric calculation, fix bug in metric calculation

---
+++ tests/evaluation_tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.bm25.evaluation import calculate_accuracy, calculate_precision
+
+
+class MetricsTestCase(unittest.TestCase):
+    def test_precision(self):
+        tp = 2
+        tn = 2
+
+        precision = calculate_precision(tp, tn)
+        self.assertAlmostEqual(precision, 0.5)
+        self.assertNotAlmostEqual(precision, 0.6)
+        self.assertNotAlmostEqual(precision, 0.51)
