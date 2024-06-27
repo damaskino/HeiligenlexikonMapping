e790c52fb24c8931b1ca6320122314d77cd0c147
chen <chenzhongchen@gmail.com>
2024-06-27 17:49:41 +0900

    update wholeset creation to shuffle entries with seed

--- src/bm25/build_trainingset.py
+++ src/bm25/build_trainingset.py
@@ -1,8 +1,11 @@
+import random
+
+
 # load and Remove the devset and goldstandard entries
 def load_goldstandard_ids():
     with open(
-            "../../data/2_annotated_data_gold_and_dev/gold_standard_hand_annotated_eschbach_li.txt",
-            "r",
+        "../../data/2_annotated_data_gold_and_dev/gold_standard_hand_annotated_eschbach_li.txt",
+        "r",
     ) as gold_standard_file:
         entry_ids = []
         for line in gold_standard_file.readlines():
@@ -15,7 +18,7 @@ def load_goldstandard_ids():
 
 def load_devset_ids():
     with open(
-            "../../data/2_annotated_data_gold_and_dev/dev_set.txt", "r"
+        "../../data/2_annotated_data_gold_and_dev/dev_set.txt", "r"
     ) as gold_standard_file:
         entry_ids = []
         for line in gold_standard_file.readlines():
@@ -26,14 +29,19 @@ def load_devset_ids():
 # Use the rulebased matches as a baseline to build a training set
 def load_rulebased_results(ids_to_remove: list):
     with open(
-            "matching_results/match_results_edit_dist_thresh_100_feast_tolerance_0_devmode_False.csv"
+        "matching_results/match_results_edit_dist_thresh_100_feast_tolerance_0_devmode_False.csv"
     ) as result_file:
+        result_list = result_file.readlines()
+        random.seed(1)
+        random.shuffle(result_list)
+        print(result_list)
+
         wholeset_string = ""
-        for match_line in result_file.readlines():
+        for match_line in result_list:
             entry_id = match_line.split(";")[0]
             match = match_line.split(";")[2].rstrip()
             entry_link = (
-                    "https://damaskino.github.io/HeiligenlexikonMapping?id=" + entry_id
+                "https://damaskino.github.io/HeiligenlexikonMapping?id=" + entry_id
             )
             match_link = "https://www.wikidata.org/wiki/" + match
 
@@ -41,11 +49,11 @@ def load_rulebased_results(ids_to_remove: list):
                 continue
             else:
                 wholeset_string += (
-                        ";".join([entry_id, match, entry_link, match_link]) + "\n"
+                    ";".join([entry_id, match, entry_link, match_link]) + "\n"
                 )
 
         with open(
-                "wholeset_match_results_edit_thresh_100_feast_0.csv", "w"
+            "wholeset_match_results_edit_thresh_100_feast_0.csv", "w"
         ) as wholeset_file:
             wholeset_file.write(wholeset_string)
 
