13f5c62c62a6547cec9a4127a9d5a76421409c04
chen <chenzhongchen@gmail.com>
2024-07-04 11:51:13 +0900

    update date cleansing

--- src/post_processing/data_validation.py
+++ src/post_processing/data_validation.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import datetime
-#Go through parsed data and look for inconsistencies
-hlex_df = pd.read_json('../../outputs_to_review/parsed_heiligenlexikon.json')
+
 
 def is_valid_date(input_date_str: str):
     if pd.isnull(input_date_str):
@@ -18,30 +17,33 @@ def is_valid_date(input_date_str: str):
     except:
         return False
 
-#Checking dates
-#print(hlex_df)
-feast_days_df = hlex_df.T[['FeastDay', 'FeastDay0', 'FeastDay1', 'FeastDay2']]
-#feast_days_df = hlex_df.T['FeastDay']
-#feast_days_df.map(check_date, na_action='ignore')
 
-counter = 0
-faulty_entries = []
-for item in feast_days_df.itertuples():
-    hlex_id = item[0]
-    for entry in item[1:]:
-        if is_valid_date(entry):
-            continue
-        else:
-            raw_entry = hlex_df.T.RawFeastDay[hlex_id]
-            if 'al' in raw_entry:
-                faulty_entries.append(";".join([hlex_id,raw_entry]))
-                print(hlex_id, ": ", entry, " - Raw:", raw_entry)
-                counter+=1
-with open('faulty_date_entries.csv', 'w', encoding='utf-8') as faulty_entries_file:
-    result_to_write = "\n".join(faulty_entries)
-    faulty_entries_file.write(result_to_write)
-print("Invalid dates found:")
-print(counter)
+if __name__ == '__main__':
+    # Go through parsed data and look for inconsistencies
+    hlex_df = pd.read_json('../../outputs_to_review/parsed_heiligenlexikon.json')
+    # Checking dates
+    # print(hlex_df)
+    feast_days_df = hlex_df.T[['FeastDay', 'FeastDay0', 'FeastDay1', 'FeastDay2']]
+    # feast_days_df = hlex_df.T['FeastDay']
+    # feast_days_df.map(check_date, na_action='ignore')
 
+    counter = 0
+    faulty_entries = []
+    for item in feast_days_df.itertuples():
+        hlex_id = item[0]
+        for entry in item[1:]:
+            if is_valid_date(entry):
+                continue
+            else:
+                raw_entry = hlex_df.T.RawFeastDay[hlex_id]
+                if 'al' in raw_entry:
+                    faulty_entries.append(";".join([hlex_id, raw_entry]))
+                    print(hlex_id, ": ", entry, " - Raw:", raw_entry)
+                    counter += 1
+    with open('faulty_date_entries.csv', 'w', encoding='utf-8') as faulty_entries_file:
+        result_to_write = "\n".join(faulty_entries)
+        faulty_entries_file.write(result_to_write)
+    print("Invalid dates found:")
+    print(counter)
 
-#TODO check for vectorized alternative
+    #TODO check for vectorized alternative
--- src/post_processing/date_cleansing.py
+++ src/post_processing/date_cleansing.py
@@ -1,7 +1,65 @@
-#basically: if a number is accompanied by either month we know the date, if there is an al or et or comma between that, we can
-#assume that the date is referring to the month after date. So essentially search from left to right, while remembering the days
-#when we find the month, the days before that can be added to the month.
+# basically: if a number is accompanied by either month we know the date, if there is an al or et or comma between that, we can
+# assume that the date is referring to the month after date. So essentially search from left to right, while remembering the days
+# when we find the month, the days before that can be added to the month.
 import pandas as pd
+import sys
+from src.post_processing.data_validation import is_valid_date
+from src.preprocessing.heiligenlexikon.parse_dates import german_date_dict
 
-faulty_entries_df = pd.read_csv('faulty_date_entries.csv', sep=';', encoding='utf-8')
-print(faulty_entries_df[1])
+date_exceptions = {"31.06.": "30.06."}
+
+
+def parse_cleaned_date_item(date_item: list):
+    days = []
+    for item in date_item:
+
+        if item.isdigit():
+            if item == "36":
+                days.append("30")
+            elif item == "2324":
+                days.append("23")
+                days.append("24")
+            elif item == "124":
+                days.append("24")
+            elif item == "51":
+                days.append("5")
+            else:
+                days.append(item)
+        else:
+            month = german_date_dict[item]
+            for day in days:
+
+                date = day + "." + month
+
+                if date in date_exceptions:
+                    date = date_exceptions[date]
+                if is_valid_date(date):
+                    print(date)
+                else:
+                    print("Invalid Date found!")
+                    print(date)
+
+            days = []
+
+
+faulty_entries_df = pd.read_csv(
+    "faulty_date_entries.csv", sep=";", encoding="utf-8", header=None
+)
+faulty_entries_series = faulty_entries_df[1]
+# print(faulty_entries_df)
+for idx, item in faulty_entries_series.items():
+    item = item.lstrip("(")
+    item = item.rstrip(")")
+    item = item.replace(".", "")
+    item = item.replace(",", "")
+    item_split = item.split(" ")
+    # print(item_split)
+
+    terms_to_filter = ["al", "u", "et", "und", "", "etc"]
+
+    filtered_split = list(filter(lambda item: item not in terms_to_filter, item_split))
+
+    print(filtered_split)
+    parse_cleaned_date_item(filtered_split)
+
+# print(faulty_entries_df[1])
