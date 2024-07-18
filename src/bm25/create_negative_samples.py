b368aee7c5d918200a2b0e89eb6f5e34b8646262
chen <chenzhongchen@gmail.com>
2024-07-19 07:34:11 +0900

    update negative example creation after feedback

--- src/bm25/create_negative_samples.py
+++ src/bm25/create_negative_samples.py
@@ -10,17 +10,21 @@ def shuffle_negative_set(hlex_df: pd.DataFrame, wiki_df: pd.DataFrame):
 
 # Draw from the full dataset by finding entries with the same name
 # and if available, the same feast day
-def find_same_name_entries(hlex_df: pd.DataFrame):
-    git_base_url = "https://damaskino.github.io/HeiligenlexikonMapping?entry="
-    hlex_df['NegativeExample'] = 0
-    hlex_df_copy = hlex_df.copy()
+def find_same_name_entries(wholeset_df: pd.DataFrame):
+    git_base_url = "https://damaskino.github.io/HeiligenlexikonMapping?id="
+    wholeset_df.insert(loc=2, column='NegativeExample', value='')
+    wholeset_df_copy = wholeset_df.copy()
     full_dataset_df = pd.read_json("../../outputs_to_review/parsed_heiligenlexikon.json")
-    hlex_df = pd.merge(hlex_df, full_dataset_df.T[['SaintName']], left_on=0, right_index=True)
-
-    wholeset_length = len(hlex_df)
+    #wholeset_df = pd.merge(wholeset_df, full_dataset_df.T[['SaintName']], left_on=0, right_index=True)
+    wholeset_df = pd.merge(wholeset_df, full_dataset_df.T, left_on=0, right_index=True)
+    wholeset_df.rename({0:'HeiligenLexikonID', 1:'WikidataID', 3:'WikidataLink'}, axis="columns", inplace=True)
+    wholeset_df.drop(2, inplace=True, axis=1)
+    wholeset_df.set_index('HeiligenLexikonID', inplace=True)
+    wholeset_df.insert(loc=2, column='NegativeExampleLink', value='')
+    wholeset_length = len(wholeset_df)
     no_double_found = 0
     negative_examples_found = []
-    for item in hlex_df_copy.itertuples():
+    for item in wholeset_df_copy.itertuples():
 
         hlex_id = item[1]
         entry = full_dataset_df[hlex_id]
@@ -39,30 +43,35 @@ def find_same_name_entries(hlex_df: pd.DataFrame):
             # name_at_index = hlex_df.iloc[index_to_replace][0]
             # print("Replacing ", name_at_index, " with ", hlex_new_id)
             # print("Adding new name")
-            hlex_df.loc[len(hlex_df) + 1] = pd.Series([hlex_new_id, git_base_url + hlex_new_id, 1, name],
-                                                      index=[0, 2, 'NegativeExample','SaintName'])
+            #wholeset_df.loc[len(wholeset_df) + 1] = pd.Series([hlex_new_id, git_base_url + hlex_new_id, 1, name],
+            #                                                  index=[0, 2, 'NegativeExample','SaintName'])
+            wholeset_df.at[hlex_id,'NegativeExample']=hlex_new_id
+            wholeset_df.at[hlex_id, 'NegativeExampleLink'] = git_base_url + hlex_new_id
+            #wholeset_df.loc[len(wholeset_df)+1] = full_dataset_df[hlex_new_id]
 
-            if len(negative_examples_found) >= int(wholeset_length / 2):
+
+
+            #if len(negative_examples_found) >= int(wholeset_length / 2):
+            if len(negative_examples_found) >= int(wholeset_length):
                 print("Found enough negatives samples!")
                 break
         else:
             no_double_found += 1
 
-    print(hlex_df)
+    print(wholeset_df)
     print("Negative examples")
     print(negative_examples_found)
     print(len(negative_examples_found))
     print("No doubles found: ", no_double_found)
-    return hlex_df
+    return wholeset_df
 
 
 # TODO filter out devset and goldstandard set entries
 if __name__ == '__main__':
     wholeset_df = pd.read_csv('wholeset_match_results_edit_thresh_100_feast_0.csv', sep=";", header=None)
-    print(wholeset_df)
-
     # hlex_columns_df = wholeset_df.loc[:, (0, 2)]
     # wiki_columns_df = wholeset_df.loc[:, (1, 3)]
 
-    positive_negative_samples_df = find_same_name_entries(hlex_df=wholeset_df)
-    positive_negative_samples_df.to_csv('positive_negative_set.csv', header=None, index=False, sep=";")
+    positive_negative_samples_df = find_same_name_entries(wholeset_df=wholeset_df)
+    positive_negative_samples_df.to_csv('positive_negative_set.csv', header=True, index=True, sep=";")
+    positive_negative_samples_df.to_json('positive_negative_set.json')
