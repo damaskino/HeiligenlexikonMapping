406eecd7bf8f0eaae67a164e98e43a77a9567de0
damaskino <chenzhongchen@gmail.com>
2024-07-19 09:06:44 +0900

    remove goldstandard and devset entries from negative sample creation

--- src/bm25/create_negative_samples.py
+++ src/bm25/create_negative_samples.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from src.bm25.build_trainingset import load_goldstandard_ids, load_devset_ids
+
 
 # Alternative way to create negative data
 def shuffle_negative_set(hlex_df: pd.DataFrame, wiki_df: pd.DataFrame):
@@ -10,11 +12,13 @@ def shuffle_negative_set(hlex_df: pd.DataFrame, wiki_df: pd.DataFrame):
 
 # Draw from the full dataset by finding entries with the same name
 # and if available, the same feast day
-def find_same_name_entries(wholeset_df: pd.DataFrame):
+def find_same_name_entries(wholeset_df: pd.DataFrame, ids_to_remove: list[str]):
+
     git_base_url = "https://damaskino.github.io/HeiligenlexikonMapping?id="
     wholeset_df.insert(loc=2, column='NegativeExample', value='')
     wholeset_df_copy = wholeset_df.copy()
     full_dataset_df = pd.read_json("../../outputs_to_review/parsed_heiligenlexikon.json")
+    full_dataset_df.drop(ids_to_remove, axis=1, inplace=True)
     #wholeset_df = pd.merge(wholeset_df, full_dataset_df.T[['SaintName']], left_on=0, right_index=True)
     wholeset_df = pd.merge(wholeset_df, full_dataset_df.T, left_on=0, right_index=True)
     wholeset_df.rename({0:'HeiligenLexikonID', 1:'WikidataID', 3:'WikidataLink'}, axis="columns", inplace=True)
@@ -71,7 +75,10 @@ if __name__ == '__main__':
     wholeset_df = pd.read_csv('wholeset_match_results_edit_thresh_100_feast_0.csv', sep=";", header=None)
     # hlex_columns_df = wholeset_df.loc[:, (0, 2)]
     # wiki_columns_df = wholeset_df.loc[:, (1, 3)]
+    gold_ids = load_goldstandard_ids()
+    dev_ids = load_devset_ids()
+    ids_to_remove = gold_ids + dev_ids
 
-    positive_negative_samples_df = find_same_name_entries(wholeset_df=wholeset_df)
+    positive_negative_samples_df = find_same_name_entries(wholeset_df=wholeset_df, ids_to_remove=ids_to_remove)
     positive_negative_samples_df.to_csv('positive_negative_set.csv', header=True, index=True, sep=";")
     positive_negative_samples_df.to_json('positive_negative_set.json')
