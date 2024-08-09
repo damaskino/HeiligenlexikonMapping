fdf3c9ce31f2b9287c292d2185773978135e0f9b
chen <chenzhongchen@gmail.com>
2024-08-09 14:08:33 +0900

    update training script to use correct data and output highest similarity scores

--- src/bm25/supervised_training.py
+++ src/bm25/supervised_training.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 import pandas as pd
+import numpy as np
 from sentence_transformers import SentenceTransformer, util
 
 print("Loading model...")
@@ -28,46 +29,58 @@ print("Done")
 #     if idx > 100:
 #         break
 
-# hlex data
-hlex_df = pd.read_json('../../outputs_to_review/parsed_heiligenlexikon.json')
+# hlex full data in case we need it
+# hlex_df = pd.read_json('../../outputs_to_review/parsed_heiligenlexikon.json')
 
 # put hlex texts into a list
 # TODO: need to be able to reassign it to their respective entries!
-hlex_texts = list(zip(hlex_df.T.index, hlex_df.T['OriginalText']))
+hlex_texts = list(zip(training_data_ids_df['HeiligenLexikonID'], training_data_ids_df['OriginalText']))
 
 # Wikidata IDs mapped to dicts of language/wikitext pairs
-wiki_text_df = pd.read_json("../../data/3_wikipedia_texts/saints_wikitexts_stub.json")
+wiki_text_df = pd.read_json("../../data/3_wikipedia_texts/saints_wikitexts.json")
 
 # go through all texts in Wikitext and create list
 # TODO: going only through German for now
 total_text_num = len(hlex_texts)
 
 # TODO: going through a fraction of the entries to iron out the details, expand to full data later
-for idx, hlex_tuple in enumerate(hlex_texts[10]):
+for idx, hlex_tuple in enumerate(hlex_texts[:]):
+    max_similarity = 0
+    best_candidate = ""
+
     hlex_id = hlex_tuple[0]
     hlex_doc = hlex_tuple[1]
     if idx % 100 == 0:
         print("At index: ", idx, "out of ", total_text_num)
     hlex_sentences = hlex_doc.split('.')
     wiki_sentences = []
+    hlex_embeddings = model.encode(hlex_sentences)
+    hlex_doc_embedding = hlex_embeddings.mean(axis=0)
 
-    # Get the German Text if available or another one
+    # Get the German Wikipedia Text if available or another one
     for item in wiki_text_df.columns:
         # print(df[item])
         if "de" in wiki_text_df[item].keys():
             wiki_german_doc = wiki_text_df[item]["de"]
+            if type(wiki_german_doc) != str:
+                continue
             wiki_sentences = wiki_german_doc.split('.')
 
-    if len(wiki_sentences) == 0:
-        continue
-    embeddings1 = model.encode(hlex_sentences)
-    embeddings2 = model.encode(wiki_sentences)
+        if len(wiki_sentences) == 0:
+            continue
 
-    # Aggregate sentence embeddings to get document embeddings
-    # Here, we use mean pooling
-    doc_embedding1 = embeddings1.mean(axis=0)
-    doc_embedding2 = embeddings2.mean(axis=0)
+        wiki_embeddings = model.encode(wiki_sentences)
 
-    # Compute cosine similarity between the two document embeddings
-    similarity = util.cos_sim(doc_embedding1, doc_embedding2)
-    # print(f"Cosine similarity between the documents: {similarity.item()}")
+        # Aggregate sentence embeddings to get document embeddings
+        # H    ere, we use mean pooling
+
+        wiki_doc_embedding = wiki_embeddings.mean(axis=0)
+
+        # Compute cosine similarity between the two document embeddings
+        similarity = util.cos_sim(hlex_doc_embedding, wiki_doc_embedding)
+        if similarity > max_similarity:
+            max_similarity = similarity
+            best_candidate = item
+
+    print(f"Best Candidate for {hlex_id}: ", best_candidate)
+    print(f"Cosine similarity between the documents: {max_similarity}")
