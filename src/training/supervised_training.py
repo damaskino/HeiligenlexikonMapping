8347015355f5fec713616594d475ed952b41c314
damaskino <chen.li@posteo.net>
2024-08-16 07:42:13 +0900

    Refactor training script

--- src/training/supervised_training.py
+++ src/training/supervised_training.py
@@ -20,7 +20,7 @@ negative_examples_df = negative_examples_df.dropna()
 negative_examples_df.rename(columns={'NegativeExample': 'HeiligenLexikonID'}, inplace=True)
 
 negative_examples_df['ShouldMatch'] = False
-positive_examples_df['Shouldmatch'] = True
+positive_examples_df['ShouldMatch'] = True
 full_training_df = pd.concat([positive_examples_df, negative_examples_df], axis=0, ignore_index=True)
 
 # put hlex texts into a list
@@ -37,17 +37,15 @@ hlex_texts_positives_df = pd.merge(positive_examples_df['HeiligenLexikonID'], hl
 
 # Wikidata IDs mapped to dicts of language/wikitext pairs
 wiki_text_df = pd.read_json("../../data/3_wikipedia_texts/saints_wikitexts.json")
-wikidata_ids = list(training_data_ids_df['WikidataID'])
-# These wikidata pages have no wikipedia pages associated with them!
-# about 58
+wikidata_ids = list(positive_examples_df['WikidataID'])
+# The following wikidata pages have no wikipedia pages associated with them! (about 58)
 wikidata_ids_without_pages = [id for id in wikidata_ids if (id not in wiki_text_df.columns)]
 wikidata_ids_with_pages = [id for id in wikidata_ids if (id in wiki_text_df.columns)]
 
-
+wiki_training_texts_df = wiki_text_df[wikidata_ids_with_pages]
 # go through all texts in Wikitext and create list
 # TODO: going only through German for now
 # total_text_num = len(hlex_texts)
-wiki_training_texts_df = wiki_text_df[wikidata_ids_with_pages]
 # NOTE: some entries from the traning data are wikidata entries only, meaning they do not have any associated
 # Wikipedia pages!
 
@@ -68,21 +66,18 @@ false_negatives = 0
 
 result_list = []
 
+wikidata_ids_without_pages = []
+wikidata_ids_no_german_page = []
+
+
+
 # converting to list for faster iteration
 full_training_list = full_training_df.to_numpy().tolist()
 training_samples_num = len(full_training_list)
 # TODO: going through a fraction of the entries to iron out the details, expand to full data later
 # for idx, hlex_tuple in enumerate(hlex_texts[:]):
-for idx, hlex_list in enumerate(full_training_list[:10]):
-    match_id = ""
-    max_similarity = 0
-    best_candidate_wiki_id = ""
-
-    # Keep track of if the current result was a true positive, true negative, etc.
-    tp = 0
-    tn = 0
-    fp = 0
-    fn = 0
+for idx, hlex_list in enumerate(full_training_list):
+    similarity = 0
 
     hlex_id = hlex_list[0]
     wikidata_id = hlex_list[1]
@@ -91,95 +86,52 @@ for idx, hlex_list in enumerate(full_training_list[:10]):
     #    print("At index: ", idx, "out of ", training_samples_num)
     print("At index: ", idx, "out of ", training_samples_num)
     hlex_doc = hlex_df[hlex_id]['OriginalText']
+    hlex_doc = hlex_doc.replace("\n", " ")
     hlex_sentences = hlex_doc.split('.')
     wiki_sentences = []
     hlex_embeddings = model.encode(hlex_sentences)
     hlex_doc_embedding = hlex_embeddings.mean(axis=0)
 
-    #TODO don't go through all the texts! only go through the ones we've matched to find a suitable threshold!
-    print("Going through wiki texts...")
 
-    for sent_idx, item in enumerate(wiki_training_texts_df.columns[:]):
-        if sent_idx % 100 == 0:
-            print("At wiki sentence: ", sent_idx)
-        # print(df[item])
-        if "de" in wiki_training_texts_df[item].keys():
-            wiki_german_doc = wiki_training_texts_df[item]["de"]
-            if type(wiki_german_doc) != str:
-                continue
+    if wikidata_id not in wiki_text_df.keys():
+        wikidata_ids_without_pages.append(wikidata_id)
+
+        continue
+
+    wikitext_series = wiki_text_df[wikidata_id]
+
+    if "de" not in wikitext_series.keys():
+        wikidata_ids_no_german_page.append(wikidata_id)
+
+    if "de" in wikitext_series.keys():
+
+        if type(wikitext_series["de"]) != str:
+            wikidata_ids_no_german_page.append(wikidata_id)
+
+        if type(wikitext_series["de"]) == str:
+            wiki_german_doc = wikitext_series["de"]
+            #todo add similarities
+            wiki_german_doc = wiki_german_doc.replace("\n", " ")
             wiki_sentences = wiki_german_doc.split('.')
+            wiki_embeddings = model.encode(wiki_sentences[:])
+            # Aggregate sentence embeddings to get document embeddings
+            # Here, we use mean pooling
+            wiki_doc_embedding = wiki_embeddings.mean(axis=0)
 
-        if len(wiki_sentences) == 0:
-            continue
+            # Compute cosine similarity between the two document embeddings
+            similarity = util.cos_sim(hlex_doc_embedding, wiki_doc_embedding)
 
-        # TODO only use a couple sentences for now
-        wiki_embeddings = model.encode(wiki_sentences[:])
-        # Aggregate sentence embeddings to get document embeddings
-        # Here, we use mean pooling
-        wiki_doc_embedding = wiki_embeddings.mean(axis=0)
+    result_str = ";".join([hlex_id,wikidata_id,str(int(should_match)),str(float(similarity))])
+    result_list.append(result_str)
+results = "\n".join(result_list)
 
-        # Compute cosine similarity between the two document embeddings
-        similarity = util.cos_sim(hlex_doc_embedding, wiki_doc_embedding)
-        print(wiki_sentences)
-        print(similarity)
-        if similarity > max_similarity:
-            max_similarity = similarity
-            best_candidate_wiki_id = item
+print("Wikidata pages without Wikipedia pages:")
+print(len(wikidata_ids_without_pages))
+print(wikidata_ids_without_pages)
 
-    if max_similarity >= threshold:
-        # if max_similarity does not reach threshold, ignore
-        print(f"Best Candidate for {hlex_id}: ", best_candidate_wiki_id)
-        print(f"Cosine similarity between the documents: {max_similarity}")
+print("Wikidata pages without German pages:")
+print(len(wikidata_ids_no_german_page))
+print(wikidata_ids_no_german_page)
 
-        total_matches += 1
-        match_id = best_candidate_wiki_id
-        # check if the identified match is correct
-        if should_match and match_id==wikidata_id:
-            true_positives += 1
-            tp = 1
-        else:
-            false_positives += 1
-            fp = 1
-
-
-    if max_similarity < threshold:
-        if should_match:
-            false_negatives += 1
-            fn = 1
-        else:
-            true_negatives += 1
-            tn = 1
-
-
-    single_match_dict = {"HLexID": hlex_id, "MatchID": match_id, "TP": tp, "TN": tn, "FP": fp, "FN": fn}
-    result_list.append(single_match_dict)
-
-filename = f"training_embedding_results_{str(threshold)}.json"
-
-with open(filename,'w') as outputfile:
-    json.dump(result_list, outputfile)
-
-print("Treshold: ", threshold)
-print("True Positives:", true_positives)
-print("True Negatives:", true_negatives)
-print("False Positives:", false_positives)
-print("False Negatives:", false_negatives)
-
-# Devset: for every entry, go through all other entries and check which one seems to be a fit, based
-# on text matching with embeddings first, and possibly other features later on
-
-# wikidata data for other features
-# saints_db = sqlite3.connect("../preprocessing/wikidata/wikidata_saints.db")
-# saints_cursor = saints_db.cursor()
-# saints = saints_cursor.executescript("SELECT * FROM saints;")
-
-# for idx, (id, content) in enumerate(saints):
-#     print(id)
-#     print(content)
-#
-#     # do something with it
-#     if idx > 100:
-#         break
-
-# hlex full data in case we need it
-# hlex_df = pd.read_json('../../outputs_to_review/parsed_heiligenlexikon.json')
+with open("training_similarities.csv", "w") as resultfile:
+    resultfile.write(results)
