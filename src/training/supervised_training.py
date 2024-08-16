18216931939a8f7a0a854e8ef00a39070709c9e2
damaskino <chen.li@posteo.net>
2024-08-16 16:25:11 +0900

    update training to allow for adding sentences flexibly across trainings

--- src/training/supervised_training.py
+++ src/training/supervised_training.py
@@ -5,10 +5,22 @@ import numpy as np
 from sentence_transformers import SentenceTransformer, util
 import datetime
 
+
+
+def find_longest_doc_language(wiki_text_series):
+    longest_doc_lang = ""
+    longest_doc_length = 0
+    for lang in wiki_text_series.keys():
+        if type(wiki_text_series[lang]) == str:
+            if longest_doc_length < len(wiki_text_series[lang]):
+                longest_doc_length = len(wiki_text_series[lang])
+                longest_doc_lang = lang
+    return longest_doc_lang
+
+
 print("Starting at: ", str(datetime.datetime.now()))
 print("Loading model...")
-# TODO: check if this model is appropriate for german data!
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
 print("Done")
 
 print("Loading training data...")
@@ -50,88 +62,83 @@ wiki_training_texts_df = wiki_text_df[wikidata_ids_with_pages]
 # Wikipedia pages!
 
 
-# TODO compute embeddings for all Hlex descriptions
+wiki_sentences_max = 10
 
+# TODO compute embeddings for all Hlex descriptions?
 
-# TODO starting with fixed threshold, make it shifting later to find best threshold
-threshold = 0.75
-threshold = 0.5
-threshold = 0.2
+for wiki_sentences_num in range(1,wiki_sentences_max):
+    result_list = []
 
-total_matches = 0
-true_positives = 0
-true_negatives = 0
-false_positives = 0
-false_negatives = 0
-
-result_list = []
-
-wikidata_ids_without_pages = []
-wikidata_ids_no_german_page = []
+    wikidata_ids_without_pages = []
+    wikidata_ids_no_german_page = []
 
 
 
-# converting to list for faster iteration
-full_training_list = full_training_df.to_numpy().tolist()
-training_samples_num = len(full_training_list)
-# TODO: going through a fraction of the entries to iron out the details, expand to full data later
-# for idx, hlex_tuple in enumerate(hlex_texts[:]):
-for idx, hlex_list in enumerate(full_training_list):
-    similarity = 0
+    # converting to list for faster iteration
+    full_training_list = full_training_df.to_numpy().tolist()
+    training_samples_num = len(full_training_list)
+    # TODO: going through a fraction of the entries to iron out the details, expand to full data later
+    # for idx, hlex_tuple in enumerate(hlex_texts[:]):
+    for idx, hlex_list in enumerate(full_training_list):
+        similarity = 0
 
-    hlex_id = hlex_list[0]
-    wikidata_id = hlex_list[1]
-    should_match = hlex_list[2]
-    # if idx % 100 == 0:
-    #    print("At index: ", idx, "out of ", training_samples_num)
-    print("At index: ", idx, "out of ", training_samples_num)
-    hlex_doc = hlex_df[hlex_id]['OriginalText']
-    hlex_doc = hlex_doc.replace("\n", " ")
-    hlex_sentences = hlex_doc.split('.')
-    wiki_sentences = []
-    hlex_embeddings = model.encode(hlex_sentences)
-    hlex_doc_embedding = hlex_embeddings.mean(axis=0)
+        hlex_id = hlex_list[0]
+        wikidata_id = hlex_list[1]
+        should_match = hlex_list[2]
+        # if idx % 100 == 0:
+        #    print("At index: ", idx, "out of ", training_samples_num)
+        print("At index: ", idx, "out of ", training_samples_num)
+        hlex_doc = hlex_df[hlex_id]['OriginalText']
+        hlex_doc = hlex_doc.replace("\n", " ")
+        hlex_sentences = hlex_doc.split('.')
+        wiki_sentences = []
+        hlex_embeddings = model.encode(hlex_sentences)
+        hlex_doc_embedding = hlex_embeddings.mean(axis=0)
 
 
-    if wikidata_id not in wiki_text_df.keys():
-        wikidata_ids_without_pages.append(wikidata_id)
+        if wikidata_id not in wiki_text_df.keys():
+            wikidata_ids_without_pages.append(wikidata_id)
 
-        continue
+            continue
 
-    wikitext_series = wiki_text_df[wikidata_id]
+        wikitext_series = wiki_text_df[wikidata_id]
 
-    if "de" not in wikitext_series.keys():
-        wikidata_ids_no_german_page.append(wikidata_id)
+        doc_lang = ""
 
-    if "de" in wikitext_series.keys():
-
-        if type(wikitext_series["de"]) != str:
+        if "de" not in wikitext_series.keys():
             wikidata_ids_no_german_page.append(wikidata_id)
+            doc_lang = find_longest_doc_language(wikitext_series)
 
-        if type(wikitext_series["de"]) == str:
-            wiki_german_doc = wikitext_series["de"]
-            #todo add similarities
-            wiki_german_doc = wiki_german_doc.replace("\n", " ")
-            wiki_sentences = wiki_german_doc.split('.')
-            wiki_embeddings = model.encode(wiki_sentences[:])
-            # Aggregate sentence embeddings to get document embeddings
-            # Here, we use mean pooling
-            wiki_doc_embedding = wiki_embeddings.mean(axis=0)
+        if "de" in wikitext_series.keys():
+            if type(wikitext_series["de"]) != str:
+                wikidata_ids_no_german_page.append(wikidata_id)
+                doc_lang = find_longest_doc_language(wikitext_series)
+            else:
+                doc_lang = "de"
 
-            # Compute cosine similarity between the two document embeddings
-            similarity = util.cos_sim(hlex_doc_embedding, wiki_doc_embedding)
+        wiki_doc = wikitext_series[doc_lang]
+        #todo add similarities
+        wiki_german_doc = wiki_doc.replace("\n", " ")
+        wiki_sentences = wiki_doc.split('.')
+        wiki_embeddings = model.encode(wiki_sentences[:wiki_sentences_num])
+        # Aggregate sentence embeddings to get document embeddings
+        # Here, we use mean pooling
+        wiki_doc_embedding = wiki_embeddings.mean(axis=0)
 
-    result_str = ";".join([hlex_id,wikidata_id,str(int(should_match)),str(float(similarity))])
-    result_list.append(result_str)
-results = "\n".join(result_list)
+        # Compute cosine similarity between the two document embeddings
+        similarity = util.cos_sim(hlex_doc_embedding, wiki_doc_embedding)
 
-print("Wikidata pages without Wikipedia pages:")
-print(len(wikidata_ids_without_pages))
-print(wikidata_ids_without_pages)
+        result_str = ";".join([hlex_id,wikidata_id,str(int(should_match)),str(float(similarity))])
+        result_list.append(result_str)
+    results = "\n".join(result_list)
 
-print("Wikidata pages without German pages:")
-print(len(wikidata_ids_no_german_page))
-print(wikidata_ids_no_german_page)
+    print("Wikidata pages without Wikipedia pages:")
+    print(len(wikidata_ids_without_pages))
+    print(wikidata_ids_without_pages)
 
-with open("training_similarities.csv", "w") as resultfile:
-    resultfile.write(results)
+    print("Wikidata pages without German pages:")
+    print(len(wikidata_ids_no_german_page))
+    print(wikidata_ids_no_german_page)
+
+    with open(f"training_similarities_first_{wiki_sentences_num}_sentences.csv", "w") as resultfile:
+        resultfile.write(results)
