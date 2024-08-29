5ba6ffd860f52702576a15eba2358183798e0cc2
chen <chenzhongchen@gmail.com>
2024-08-29 16:36:24 +0900

    refactoring similarity cutoff tuning to find max acc settings

--- src/training/similarity_cutoff_tuning.py
+++ src/training/similarity_cutoff_tuning.py
@@ -2,14 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score
 
-# training_df = pd.read_csv("training_similarities.csv", sep=";", header=None)
-training_df = pd.read_csv("training_similarities_first_3_sentences.csv", sep=";", header=None)
-print(training_df)
-training_df.columns = ['HLexID', 'WikidataID', 'ShouldMatch', 'Similarity']
 
-
-for threshold in np.arange(0,1.0, 0.01):
-
+def calculate_acc_for_similarity_cutoff(training_df, threshold):
     matches_series = training_df['Similarity'].where(training_df['Similarity'] < threshold, 1)
     matches_series.where(matches_series > threshold, 0, inplace=True)
     matches_series = matches_series.astype(int)
@@ -18,8 +12,39 @@ for threshold in np.arange(0,1.0, 0.01):
     gold_matches = training_df['ShouldMatch']
     system_matches = training_df['SystemMatch']
 
-
     acc = accuracy_score(gold_matches, system_matches)
+
+
+def evaluate_files():
+    # training_df = pd.read_csv("training_similarities.csv", sep=";", header=None)
+    max_acc = 0
+    max_sent_length = 0
+    max_sim_cut_off = 0
+
+    for sent_length in range(1, 10):
+
+        file_name = f"training_similarities_first_{sent_length}_sentences.csv"
+
+        print(file_name)
+
+        training_df = pd.read_csv(file_name, sep=";", header=None)
+        print(training_df)
+        training_df.columns = ['HLexID', 'WikidataID', 'ShouldMatch', 'Similarity']
+
+        for threshold in np.arange(0.5, 1.0, 0.01):
+            acc = calculate_acc_for_similarity_cutoff(threshold)
+            if acc > max_acc:
+                max_acc = acc
+                max_acc_sim_thresh = threshold
+                max_acc_sent_length = sent_length
+
+    print("Best Accuracy:", max_acc)
+    print("Highest Acc at similarity: ", max_sim_cut_off)
+    print("Highest Acc at sentence Length: ", max_sent_length)
     # Add precision recall
-    #check acc, threshold 0 or 1 should yield 50% acc!
-    print(threshold, acc)
+    # check acc, threshold 0 or 1 should yield 50% acc!
+    # print(threshold, acc)
+
+
+if __name__ == '__main__':
+    evaluate_files()
