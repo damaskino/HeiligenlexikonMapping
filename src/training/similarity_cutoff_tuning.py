bee349237dd509c3053fb75ea0bdfbd393846093
damaskino <chen.li@posteo.net>
2024-09-01 02:39:59 +0900

    merge previous changes to include accuracies for MAX sentence length

--- src/training/similarity_cutoff_tuning.py
+++ src/training/similarity_cutoff_tuning.py
@@ -15,42 +15,57 @@ def calculate_acc_for_similarity_cutoff(training_df, threshold):
     acc = accuracy_score(gold_matches, system_matches)
     return acc
 
+def load_file_to_training_df(file_name):
+    training_df = pd.read_csv(file_name, sep=";", header=None)
+    training_df.columns = ['HLexID', 'WikidataID', 'ShouldMatch', 'Similarity']
+    return training_df
 
 def evaluate_files():
     # training_df = pd.read_csv("training_similarities.csv", sep=";", header=None)
 
     #Keep track of the max accuracy we've seen
     max_acc = 0
-    max_sent_length = 0
-    max_sim_cut_off = 0
+    max_acc_sent_length = 0
+    max_acc_sim_thresh = 0
 
     acc_results = []
 
 
+
+    #Compute for varying sentence lengths
     for sent_length in range(1, 10):
 
         file_name = f"training_similarities_first_{sent_length}_sentences.csv"
-
+        training_df = load_file_to_training_df(file_name)
         print(file_name)
-
-        training_df = pd.read_csv(file_name, sep=";", header=None)
-        print(training_df)
-        training_df.columns = ['HLexID', 'WikidataID', 'ShouldMatch', 'Similarity']
 
         for threshold in np.arange(0.5, 1.0, 0.01):
             acc = calculate_acc_for_similarity_cutoff(training_df, threshold)
-
             result_tuple = (str(sent_length), str(round(Decimal(threshold), 2)), str(acc))
             acc_results.append(";".join(result_tuple))
+
 
             if acc > max_acc:
                 max_acc = acc
                 max_acc_sim_thresh = threshold
                 max_acc_sent_length = sent_length
 
+    #Compute for full sentences
+    file_name = f"training_similarities_all_sentences.csv"
+    training_df = load_file_to_training_df(file_name)
+    for threshold in np.arange(0.5, 1.0, 0.01):
+        acc = calculate_acc_for_similarity_cutoff(training_df, threshold)
+        result_tuple = ("MAX", str(round(Decimal(threshold), 2)), str(acc))
+        acc_results.append(";".join(result_tuple))
+        if acc > max_acc:
+            max_acc = acc
+            max_acc_sim_thresh = threshold
+            max_acc_sent_length = "MAX"
+
+
     print("Best Accuracy:", max_acc)
-    print("Highest Acc at similarity: ", max_sim_cut_off)
-    print("Highest Acc at sentence Length: ", max_sent_length)
+    print("Highest Acc at similarity: ", max_acc_sim_thresh)
+    print("Highest Acc at sentence Length: ", max_acc_sent_length)
 
     # Add precision recall
     # check acc, threshold 0 or 1 should yield 50% acc!
