231f0bfd58e3d85f69041184f41a052935bf7844
damaskino <chenzhongchen@gmail.com>
2024-08-31 18:02:10 +0900

    produce result file for accuracy depending on sentence length and similarity parameters for further processing into graphs

--- src/training/similarity_cutoff_tuning.py
+++ src/training/similarity_cutoff_tuning.py
@@ -13,13 +13,19 @@ def calculate_acc_for_similarity_cutoff(training_df, threshold):
     system_matches = training_df['SystemMatch']
 
     acc = accuracy_score(gold_matches, system_matches)
+    return acc
 
 
 def evaluate_files():
     # training_df = pd.read_csv("training_similarities.csv", sep=";", header=None)
+
+    #Keep track of the max accuracy we've seen
     max_acc = 0
     max_sent_length = 0
     max_sim_cut_off = 0
+
+    acc_results = []
+
 
     for sent_length in range(1, 10):
 
@@ -32,7 +38,11 @@ def evaluate_files():
         training_df.columns = ['HLexID', 'WikidataID', 'ShouldMatch', 'Similarity']
 
         for threshold in np.arange(0.5, 1.0, 0.01):
-            acc = calculate_acc_for_similarity_cutoff(threshold)
+            acc = calculate_acc_for_similarity_cutoff(training_df, threshold)
+
+            result_tuple = (str(sent_length), str(threshold), str(acc))
+            acc_results.append(";".join(result_tuple))
+
             if acc > max_acc:
                 max_acc = acc
                 max_acc_sim_thresh = threshold
@@ -41,9 +51,16 @@ def evaluate_files():
     print("Best Accuracy:", max_acc)
     print("Highest Acc at similarity: ", max_sim_cut_off)
     print("Highest Acc at sentence Length: ", max_sent_length)
+
     # Add precision recall
     # check acc, threshold 0 or 1 should yield 50% acc!
     # print(threshold, acc)
+    #Write parameter and accuracy to result file
+    results_string = "\n".join(acc_results)
+    header = "SentenceLength;SimilarityThreshold;Accuracy\n"
+    results_string = header+results_string
+    with open("acc_results_across_sent_len_and_similarity_cutoff.csv", "w") as acc_result_file:
+        acc_result_file.write(results_string)
 
 
 if __name__ == '__main__':
